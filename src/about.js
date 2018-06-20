e70dbdc82c1af462110728fcfd3c3cd870a88b51
Ryan <ryangrahamwpb@gmail.com>
2018-06-20 13:35:32 -0500

    Add about section html.

--- src/about.js
+++ src/about.js
@@ -11,8 +11,13 @@ class About extends Component {
         <div className="titleDesc">
          <p>this my description paragraph.</p>
         </div>
-        <div className="aboutImage">
-          
+        <div className="col">
+          <div className="col col-left">
+           <p class="p1"><span class="s1">Our IOP West Palm Beach program develops individualized drug abuse and alcohol abuse treatments for our patients utilizing individual and group therapies. The at A New Start intensive outpatient program staff has extensive knowledge and experience working with:</span></p>
+         </div>
+        <div className="col col-right">
+          <p class="p1"><span class="s1">Therapies at A New Start for drug abuse treatment and alcohol addiction include:&nbsp;Rational Emotive Behavioral Therapy, Cognitive-Behavioral Therapy, Client-Centered Therapy, Trauma Therapy, Relapse Prevention, Family Systems Therapy, 12-Step Recovery Program, Existential Therapy, Dual Diagnosis Therapy, Solution-Focused Therapy.</span></p>
+         </div>
         </div>
         
       </section>
