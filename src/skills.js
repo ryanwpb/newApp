54e002e18f4920e7b0a641e97e0f0c721e45b7be
Ryan <ryangrahamwpb@gmail.com>
2018-06-22 16:18:14 -0500

    Add images to skill boxes.

--- src/skills.js
+++ src/skills.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Logo from './assets/images/logo.png';
+import htmlImg from './assets/images/html-img.png';
+import cssImg from './assets/images/css-image.png';
 
 
 class Skills extends Component {
@@ -7,11 +8,13 @@ class Skills extends Component {
     return (
       <section id="skills">
         <div className="skillBox">
+        <img src={htmlImg} width="" alt="HTML screen shot"/>
           <h3>HTML</h3>
          <p>Shortdescription to go here.</p>
           <span>Read more</span>
         </div>
          <div className="skillBox">
+         <img src={cssImg} width="" alt="CSS screen shot"/>
           <h3>CSS</h3>
          <p>Shortdescription to go here.</p>
          <span>Read more</span>
