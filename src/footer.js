e690888fceda1a135aa9f8a25248e7cbdca08223
Ryan <ryangrahamwpb@gmail.com>
2018-06-24 14:39:57 -0500

    Add contact and logo to footer.

--- src/footer.js
+++ src/footer.js
@@ -4,13 +4,23 @@ class Footer extends Component {
     render() {
         return (
         <section className="footer">
-            <p>I am a footer.</p>
-            <div className="icon-area">
-            <i class="icon ion-logo-github"></i>
-            <i class="icon ion-logo-linkedin"></i>
-            <i class="icon ion-logo-facebook"></i>
-            <i class="icon ion-logo-instagram"></i>
+            <div className="footer-container">
+              <div className="footer-logo">
+                <h2>Ryan Graham</h2>
+              </div>
+              <div className="footer-contact">
+              <h3>Contact</h3>
+                            
+              </div>
+              <div className="footer-social">
+                <h3>Social Media</h3>
+                <i class="icon ion-logo-github"></i>
+                <i class="icon ion-logo-linkedin"></i>
+                <i class="icon ion-logo-facebook"></i>
+                <i class="icon ion-logo-instagram"></i>
+              </div>
             </div>
+            
         </section>
         )
     }
